09688598646d12679fb07a769f1c771ab9f7c404
emilyyy9876 <strongwomankky@gmail.com>
2024-08-06 01:22:55 +0900

    sentinel_node, node_left_rotate, node_right_rotate, rbtree_insert_fixup 생성, rbtree_insert 수정

--- src/rbtree.c
+++ src/rbtree.c
@@ -1,31 +1,78 @@
 #include "rbtree.h"
-
 #include <stdlib.h>
+
+// 새로 만든 함수들
+int rbtree_insert_fixup(rbtree *t, node_t *cur_node);
+node_t node_left_rotate(rbtree *t, node_t *cur_node);
+node_t node_right_rotate(rbtree *t, node_t *cur_node);
 
 rbtree *new_rbtree(void)
 {
   rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
 
-  // TODO: initialize struct if needed
-  // p를 초기화 안해도 됨??--calloc이니까?
-
-  if (p->root != NULL || p->nil != NULL)
-  {
-    p->root = NULL;
-    p->nil = NULL;
-  }
+  // TODO: initialize struct if neededa
 
   // sentinel node 만들기
-  p->nil = (node_t *)calloc(1, sizeof(node_t));
-  // root를 sentinel node로 설정
-  p->root = p->nil;
-  // p->nill을 정의하기--sentinel node의 p,left, right,key등은 수행과정에서 값이 채워질 수 있으나 의미를 갖지 않음
-  p->nil->color = RBTREE_BLACK;
-  p->nil->key = NULL;
-  p->nil->parent = NULL;
-  p->nil->left = NULL;
+  node_t *sentinel_node = (node_t *)calloc(1, sizeof(node_t));
 
+  // root를 sentinel node로 설정
+  p->nil = sentinel_node;
+  p->root = p->nil;
+
+  sentinel_node->color = RBTREE_BLACK;
+  // p->nil->key = NULL;
+  sentinel_node->parent = NULL;
+  sentinel_node->left = NULL;
+  sentinel_node->right = NULL;
   return p;
+}
+
+node_t node_left_rotate(rbtree *t, node_t *cur_node)
+{
+  node_t *new_parent = cur_node->right;
+  cur_node->right = new_parent->left;
+  if (new_parent->left != t->nil)
+  {
+    new_parent->left->parent = cur_node;
+  }
+  new_parent->parent = cur_node->parent;
+  if (cur_node->parent == t->nil)
+  {
+    t->root = new_parent;
+  }
+  else if (cur_node = cur_node->parent->left)
+  {
+    cur_node->parent->left = new_parent;
+  }
+  else
+  {
+    cur_node->parent->right = new_parent;
+  }
+  // return 값으로??
+}
+
+node_t node_right_rotate(rbtree *t, node_t *cur_node)
+{
+  node_t *new_parent = cur_node->left;
+  cur_node->left = new_parent->right;
+  if (new_parent->right != t->nil)
+  {
+    new_parent->right->parent = cur_node;
+  }
+  new_parent->parent = cur_node->parent;
+  if (cur_node->parent == t->nil)
+  {
+    t->root = new_parent;
+  }
+  else if (cur_node = cur_node->parent->left)
+  {
+    cur_node->parent->left = new_parent;
+  }
+  else
+  {
+    cur_node->parent->right = new_parent;
+  }
+  // return 값으로??
 }
 
 void delete_rbtree(rbtree *t)
@@ -33,17 +80,107 @@ void delete_rbtree(rbtree *t)
   // TODO: reclaim the tree nodes's memory
   free(t);
 }
+int rbtree_insert_fixup(rbtree *t, node_t *cur_node)
+{
+  node_t *uncle_node = (node_t *)calloc(1, sizeof(node_t));
+  while (cur_node->parent->color == RBTREE_RED)
+  {
+    if (cur_node->parent == cur_node->parent->parent->left)
+    {
+      uncle_node = cur_node->parent->parent->right;
+      if (uncle_node->color == RBTREE_RED)
+      {
+        cur_node->parent->color = RBTREE_BLACK;
+        uncle_node->color = RBTREE_BLACK;
+        cur_node->parent->parent = RBTREE_RED;
+        cur_node = cur_node->parent->parent;
+      }
+      // cur_node가 부모보드와 꺾인 변에 있을 때:
+      else if (cur_node == cur_node->parent->right)
+      {
+        cur_node = cur_node->parent;
+        node_t node_left_rotate(rbtree * t, node_t * cur_node);
+      }
+      // cur_node가 부모노드와 같은변에 있을 때:
+      cur_node->parent->color = RBTREE_BLACK;
+      cur_node->parent->parent->color = RBTREE_RED;
+      node_t node_right_rotate(rbtree * t, node_t * cur_node);
+    }
+    else
+    {
+      uncle_node = cur_node->parent->parent->left;
+      if (cur_node->parent == cur_node->parent->parent->right)
+      {
+        uncle_node = cur_node->parent->parent->left;
+      }
+      if (uncle_node->color == RBTREE_RED)
+      {
+        cur_node->parent->color = RBTREE_BLACK;
+        uncle_node->color = RBTREE_BLACK;
+        cur_node->parent->parent = RBTREE_RED;
+        cur_node = cur_node->parent->parent;
+      }
+      else if (cur_node == cur_node->parent->left)
+      {
+        cur_node = cur_node->parent;
+        node_t node_right_rotate(rbtree * t, node_t * cur_node);
+      }
+    }
+  }
+  return 0;
+}
 
 node_t *rbtree_insert(rbtree *t, const key_t key)
 {
   // TODO: implement insert
 
+  // 새로운 노드 만들기
+  node_t *new_node = (node_t *)calloc(1, sizeof(node_t));
+  new_node->key = key;
+  new_node->color = RBTREE_RED;
+
+  node_t *parent_node = t->nil;
+  node_t *cur_node = t->root;
+
+  while (cur_node != t->nil)
+  {
+    parent_node = cur_node;
+    if (new_node->key < cur_node->key)
+    {
+      cur_node = cur_node->left;
+    }
+    else
+    {
+      cur_node = cur_node->right;
+    }
+  }
+  new_node->parent = parent_node;
+
+  if (parent_node == t->nil)
+  {
+    t->root = new_node;
+  }
+  else if (new_node->key < parent_node->key)
+  {
+    parent_node->left = new_node;
+  }
+  else
+  {
+    parent_node->right = new_node;
+  }
+  new_node->left = t->nil;
+  new_node->right = t->nil;
+  new_node->color = RBTREE_RED;
+
+  int rbtree_insert_fixup(rbtree * t, node_t * new_node);
+  t->root->color = RBTREE_BLACK;
   return t->root;
 }
 
 node_t *rbtree_find(const rbtree *t, const key_t key)
 {
   // TODO: implement find
+
   return t->root;
 }
 
