ba0a3772a42bea43ade513ec694f2a010004489a
emilyyy9876 <strongwomankky@gmail.com>
2024-08-05 16:49:47 +0900

    /테스트를 위한 임시 main 함수 코드 삭제

--- src/rbtree.c
+++ src/rbtree.c
@@ -70,13 +70,3 @@ int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n)
   // TODO: implement to_array
   return 0;
 }
-
-// 테스트를 위한 main 함수
-int main(void)
-{
-  rbtree *tree = new_rbtree();
-
-  // 트리 삭제 (메모리 해제)
-  delete_rbtree(tree);
-  return 0;
-}
